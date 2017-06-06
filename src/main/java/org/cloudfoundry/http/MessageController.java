aeca73b9966814fa2786914ab56e3b78d96240d1
AMS\daniel.stashek <daniel.stashek@cgi.com>
2017-06-06 16:40:16 -0600

    added message and bootstrap app

---
+++ src/main/java/org/cloudfoundry/http/MessageController.java
@@ -0,0 +1,27 @@
+package org.cloudfoundry.http;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.cloudfoundry.CloudConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+class MessageController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return "\n" + this.message + "\n\n";
+    }
+}
